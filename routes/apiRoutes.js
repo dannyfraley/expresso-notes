b4b719705d1c77f35e00af265de148472bb1952d
Danny Fraley <dannyfraley@gmail.com>
2020-11-03 13:32:58 -0500

    fixed post and delete routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,38 @@
 var router = require("express").Router();
 var db = require("../db/db.json");
+var uuid = require("uuid");
+var fs = require('fs');
 
-router.get("/api/notes", function(req,res){
+router.get("/api/notes", function (req, res) {
     res.json(db)
 })
+
+router.post("/api/notes", function (req, res) {
+    var newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuid.v4()
+    }
+
+    fs.readFile("./db/db.json", (err, data) => {
+        var jsonNote = JSON.parse(data);
+        jsonNote.push(newNote);
+        fs.writeFile("./db/db.json", JSON.stringify(jsonNote), function (err, result) {
+            if (err) console.log('error', err);
+        });
+    });
+    res.send("Note created");
+});
+
+router.delete("/api/notes/:id", function (req, res) {
+    fs.readFile("./db/db.json", (err, data) => {
+        var jsonNote = JSON.parse(data);
+        var newNote = jsonNote.filter(note => note.id !== req.params.id);
+        fs.writeFile("./db/db.json", JSON.stringify(newNote), function (err, result) {
+            if (err) console.log('error', err);
+        });
+    });
+    res.send("Note deleted");
+});
 
 module.exports = router
